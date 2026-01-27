996bb114daae23911668a8d818a5a3683b07d3f1
Elliott Romano <47725691+elliott-romano@users.noreply.github.com>
2026-01-27 14:35:58 -0500

    initial push to git
    added my files

---
+++ sketch.js
@@ -0,0 +1,64 @@
+let img
+
+let size; 
+
+let asciiChar = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,^`'. "
+
+function preload(){
+  img = loadImage("images/01.jpg")
+}
+
+
+function setup() {
+  createCanvas(400, 400);
+  
+  img.resize(80,0)
+  
+  size = width / img.width;
+  
+  // print(size)
+  
+  
+}
+
+function draw() {
+  background(220);
+  
+  
+  
+  
+  image(img,0,0)
+  
+  for (let i = 0; i< img.width; i++ ){
+    for (j = 0; j< img.height; j++){
+      let pixelVal = img.get(i,j);
+      // print(pixelVal)
+      // noLoop();
+      let c = brightness(pixelVal);
+      // print(c);
+      // noLoop();
+    
+      let tIndex = floor(map(c ,0, 100, 0, asciiChar.length))
+      // print(tIndex);
+      // noLoop();
+      
+      let x = i *size + size/2;
+      let y = j * size + size/2;
+    
+      t = asciiChar.charAt(tIndex)
+      
+      
+      textSize(size)
+      
+      
+      textAlign(CENTER, CENTER);
+      text(t, x , y);
+      
+      
+    }
+  }
+  
+  
+  
+  
+}
